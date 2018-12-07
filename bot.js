0487bd39e46d739b91ab1c72821a85a6a1fee2ca
TheWolfBane154 <45442131+TheWolfBane154@users.noreply.github.com>
2018-12-06 20:39:59 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+var bot = new Discord.Client();
+const fs = require("fs");
+var prefix = "";
+
+fs.readdir("./commands/", (err, files) => {
+  console.log("Loading commands...");
+  if (err) return console.log(`Command loading failed!`);
+  files.filter(f => f.split(".").pop() === "js").forEach((f, i) => {
+    bot.commands.set(require(`./commands/${f}`).help.name, require(`./commands/${f}`));
+  });
+});
+
+bot.on("ready", () => {
+console.log("Alive boi")
+})
+
+
+
+bot.login(process.env.TOKEN)
