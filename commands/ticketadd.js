d7037f2b56d1a0bc433a02ccd7aff2c2a14e568e
TheWolfBane154 <45442131+TheWolfBane154@users.noreply.github.com>
2018-12-08 14:29:20 -0500

    Create ticketadd.js

---
+++ commands/ticketadd.js
@@ -0,0 +1,12 @@
+module.exports.run(bot, message, args, Discord) => {
+let server = message.guild
+let number = Math.floor(Math.random) * 1000
+
+let em = new Discord.RichEmbed()
+.setTitle("Ticket Created")
+.setDescription("Think you for createing an offer for a product!")
+.addField("Ticket number:", number)
+
+message.channel.send({embed: em})
+server.createChannel(text, number)
+}
