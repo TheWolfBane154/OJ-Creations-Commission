6d2e341bd27e0443d0de4a76574aa435a0e74c20
TheWolfBane154 <45442131+TheWolfBane154@users.noreply.github.com>
2018-12-23 11:29:51 -0500

    Create contact.js

---
+++ commands/contact.js
@@ -0,0 +1,22 @@
+module.exports.run = (bot, message, args, Discord) => {
+let joinargs = args.join(" ")
+
+let em = new Discord.RichEmbed()
+.setTitle("Contacting Owner")
+.setDescription(`Contacting owner for reason: ${joinargs}`)
+.setColor("BLUE")
+message.channel.send({embed: em})
+
+let emb = new Discord.RichEmbed()
+.setTitle("You got mail")
+.setDescription(`Message from ${message.author.username}, ID: ${message.author.id}`)
+.addField("Reason for contact:", joinargs)
+.setColor("GREEN")
+
+bot.users.get("369256915479560192").send({embed: emb})
+
+}
+
+module.exports.help = {
+  name: "contact"
+  }
