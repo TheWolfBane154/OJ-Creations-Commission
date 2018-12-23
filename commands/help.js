d1569aeb4df55775c50435bf4ff556a93d0b3080
TheWolfBane154 <45442131+TheWolfBane154@users.noreply.github.com>
2018-12-23 11:26:07 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,13 +1,1 @@
-module.exports.run = (bot, message, args, Discord) => {
 
-let em = new Discord.RichEmbed()
-.setTitle("OJ Creations Bot Help menu!")
-.setColor("ORANGE")
-.setDescription("Commands will be shown here ^-^")
-.setFooter("This bot was made by: Bane#3927")
- console.log("Hello!")
-message.channel.send({embed: em})
-}
-module.exports.help = {
-  "name": "help"
-}
